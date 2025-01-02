c576fad1910dd84d7e5db66f0f03c2493b8430d3
electric-otter <krw122081@gmail.com>
2025-01-01 20:58:48 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='designui',
+    version='1.0 (2025)',
+    packages=find_packages(),
+    install_requires=[],
+    author='electric-otter',
+    author_email='electric-otter-github@proton.me',
+    description='The UI framework thats stable.',
+    url='https://github.com/electric-otter/DesignUI/tree/main',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General public license 3.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
